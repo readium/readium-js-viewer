4e47d19317dfa956a78bffbc6571bd54cd6e8731
justinHume <justin.df.hume@gmail.com>
2013-06-12 11:42:20 -0700

    Removed a bunch of unused dependencies now provided by a CDN; updated the sample to generate the library list from a json file; added to the Readme; added a task to the Rakefile to get the latest version of SimpleReadium.js from the Readium.js repository.

--- modules_and_dependencies/DemoAppCode.js
+++ modules_and_dependencies/DemoAppCode.js
@@ -3,11 +3,23 @@ RWCDemoApp.getInputValue = function (inputId) {
 };
 
 RWCDemoApp.setModuleContainerHeight = function () {
-    $("#epub-reader-container").css({ "height" : $(document).height() * 0.85 + "px" });
+    $("#epub-reader-container").css({ "height" : $(window).height() * 0.85 + "px" });
 };
 
 RWCDemoApp.parseXMLFromDOM = function (data) {
     var serializer = new XMLSerializer();
     var packageDocumentXML = serializer.serializeToString(data);
     return packageDocumentXML;
+};
+
+RWCDemoApp.addLibraryList = function ($ulElementContainer, libraryJson) {
+
+    _.each(libraryJson.library_epubs, function (currEpub) {
+
+        var $currLi = $('<li><a id="' + currEpub.url_to_package_doc + '" href="#">' + currEpub.title + '</a></li>');
+        $currLi.on("click", function () {
+            RWCDemoApp.loadAndRenderEpub(currEpub.url_to_package_doc);
+        });
+        $ulElementContainer.append($currLi);
+    });
 };
