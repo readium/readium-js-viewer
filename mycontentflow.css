ce02e8cba5ab9cd35708dfc2ff8e943f6e175aa2
Ric Wright <rkwright@geofx.com>
2013-10-25 12:17:17 -0500

    Adding missing files.  Also removed extraneous debug statement from library.html

---
+++ mycontentflow.css
@@ -0,0 +1,120 @@
+/* ========== ContentFlow ========== */
+/*
+ * Within this file you can adjust the styling of ContentFlow
+ * to your personal needs. The default styling is the same as found on the 
+ * project page.
+ *
+ * To utilze this file for global styleing, rename it to 'mycontentflow.css'.
+ */
+
+.ContentFlow {
+}
+
+    /* ----- styling of items ----- */
+    .ContentFlow .flow .item.active {
+        cursor: pointer;
+    }
+        .ContentFlow .flow .item .caption {
+            font-size: 100%;
+            font-weight: bold;
+            text-align: center;
+            color: white;
+            max-height: 30%;
+            bottom: 10%;
+            background: url(img/1x1_0.5_black.png);
+            width: 100%;
+        }
+        * html .ContentFlow .flow .item .caption {
+            background-image: none;
+            filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(enabled=true, sizingMethod=scale, src='img/1x1_0.5_black.png');
+        }
+            .ContentFlow .flow .item .caption a,
+            .ContentFlow .flow .item .caption a:link,
+            .ContentFlow .flow .item .caption a:visited,
+            .ContentFlow .flow .item .caption a:active,
+            .ContentFlow .flow .item .caption a:hover {
+                text-decoration: none;
+                color: white;
+                font-style: italic;
+                font-size: 0.8em;
+            }
+            .ContentFlow .flow .item .caption a:hover {
+                text-decoration: underline;
+            }
+        .ContentFlow .flow .item.active .caption {
+            /*display: block;*/ /* uncomment to show caption inside item */
+        }
+
+    /* ----- scrollbar ----- */
+    .ContentFlow .scrollbar {
+        width: 50%;
+        margin: 0px auto;
+        margin-top: 10px;
+        height: 16px;
+        background: url(img/scrollbar_white.png) left center repeat-x;
+        position: relative;
+    }
+
+        .ContentFlow .scrollbar .slider {
+            width: 16px;
+            height: 16px;
+            background: url(img/slider_white.png) center center no-repeat;
+        }
+        
+        /* only for IE <= 6 and a alphatransparent slider image */
+        * html .ContentFlow .scrollbar .slider {
+            background-image: none;
+            filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(enabled=true, sizingMethod=crop, src='img/slider_white.png');
+        }
+            .ContentFlow .scrollbar .slider .position {
+                top: 120%;
+                font-size: 16px;
+                font-weight: bold;
+                color: silver
+            }
+
+    /* ----- global caption ----- */
+    .ContentFlow .globalCaption {
+        text-align: center;
+        font-weight: bold;
+        color: white;
+        font-size: 14px;
+        height: 20px;
+        margin: 2em auto;
+    }
+        .ContentFlow .globalCaption .caption {
+        }
+            .ContentFlow .globalCaption .caption a,
+            .ContentFlow .globalCaption .caption a:link,
+            .ContentFlow .globalCaption .caption a:visited,
+            .ContentFlow .globalCaption .caption a:active,
+            .ContentFlow .globalCaption .caption a:hover {
+                text-decoration: none;
+                color: white;
+                font-style: italic;
+                font-size: 0.8em;
+            }
+
+            .ContentFlow .globalCaption .caption a:hover {
+                text-decoration: underline;
+            }
+
+    /* ----- load indicator ----- */
+    .ContentFlow .loadIndicator {
+        width: 100%;
+        height: 100%;
+        top: 0px;
+        left: 0px;
+        background: black;
+    }
+        .ContentFlow .loadIndicator .indicator {
+            background: url(img/loader.gif) center center no-repeat;
+            width: 100%;
+            height: 100%;
+        }
+        * html .ContentFlow .loadIndicator .indicator {
+            height: 100px;
+        }
+
+/* ================================= */
+
