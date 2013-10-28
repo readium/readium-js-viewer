ceef1aaaf9790bd366a73092821a4ea50314831e
Boris Schneiderman <boriss@evidentpoint.com>
2013-10-28 14:02:01 -0700

    Replaced Readium-web-componnents with readium-js

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,11 +16,11 @@ module.exports = function (grunt) {
     },
     curl: {
       Readium: {
-        src: 'https://raw.github.com/readium/Readium-Web-Components/master/epub-modules/readium-js/out/Readium.js',
+        src: 'https://raw.github.com/readium/readium-js/master/epub-modules/readium-js/out/Readium.js',
         dest: __dirname + '/lib/Readium.js'
       },
       ReadiumSync: {
-        src: 'https://raw.github.com/readium/Readium-Web-Components/master/epub-modules/readium-js/out/Readium.syncload.js',
+        src: 'https://raw.github.com/readium/readium-js/master/epub-modules/readium-js/out/Readium.syncload.js',
         dest: __dirname + '/lib/Readium.syncload.js'
       }
     }
