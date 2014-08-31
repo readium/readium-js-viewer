2d577ead3815996b0974a0ef0714300b5373063f
Daniel Weck <daniel.weck@gmail.com>
2014-08-31 20:53:33 +0100

    Fixes #185 Regression: keyboard shortcuts fail when focus is within content iframe (cloud reader only, not chrome app)

--- extend_require_config.js
+++ extend_require_config.js
@@ -11,9 +11,10 @@
 //  used to endorse or promote products derived from this software without specific 
 //  prior written permission.
 
-//This file can be used to modify or extend require.js configuration object
-//tha is already loaded from readium-js/require_config.js or from require_config_compact.js
-
-//for example to add path to some package:
-//requirejs.paths.some_package = 'lib/my_extra_package'
-
+require.config({
+	config : {
+    	'EpubReader' : {
+            'useSimpleLoader' : true
+    	}
+    }
+});
