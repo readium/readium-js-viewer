35dc4ff869a2fed9573c78b4ca44fdf4b6d8d4cd
Ryan Ackley <ryanackley@gmail.com>
2015-04-01 15:08:33 -0400

    remove testing code

--- chrome-app/epubReadingSystem.js
+++ chrome-app/epubReadingSystem.js
@@ -1,10 +1,3 @@
-var windowAddEventListener = Window.prototype.addEventListener;
-Window.prototype.addEventListener = function(type) {
-  if (type === 'unload' || type === 'beforeunload') 
-	return true;
-  else
-    return windowAddEventListener.apply(window, arguments);
-};
 navigator.epubReadingSystem = window.parent.navigator.epubReadingSystem;
 window.parent = window.self;
 window.top = window.self;
