4ccf4db4e28c157912b9bfe35166d65a2f0a3deb
Ryan Ackley <ryanackley@gmail.com>
2015-04-01 13:48:33 -0400

    Updates manifest to proper packaged app

--- chrome-app/epubReadingSystem.js
+++ chrome-app/epubReadingSystem.js
@@ -1,3 +1,10 @@
+var windowAddEventListener = Window.prototype.addEventListener;
+Window.prototype.addEventListener = function(type) {
+  if (type === 'unload' || type === 'beforeunload') 
+	return true;
+  else
+    return windowAddEventListener.apply(window, arguments);
+};
 navigator.epubReadingSystem = window.parent.navigator.epubReadingSystem;
 window.parent = window.self;
 window.top = window.self;
