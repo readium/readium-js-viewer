2c8d627db8a69a2f3cbb98cf16fefa46b3cb0fba
danielweck <daniel.weck@gmail.com>
2015-09-30 16:57:34 +0100

    fixes https://github.com/readium/readium-js-viewer/issues/415 graceful degradation when EPUB fails to load (+ added try/catch on the Media Overlays pause action, as this can prevent going back into the library view when an ebook has failed to load)

--- src/js/EpubReader.js
+++ src/js/EpubReader.js
@@ -71,7 +71,16 @@ Helpers){
     // TODO: is this variable actually used anywhere here??
     // (bad naming convention, hard to find usages of "el")
     var el = document.documentElement;
+
+    function setBookTitle(title) {
     
+        var $titleEl = $('.book-title-header');
+        if ($titleEl.length) {
+            $titleEl.text(title);
+        } else {
+            $('<h2 class="book-title-header"></h2>').insertAfter('.navbar').text(title);
+        }
+    };
 
     // This function will retrieve a package document and load an EPUB
     var loadEbook = function (readerSettings, openPageRequest) {
@@ -81,6 +90,20 @@ Helpers){
             ebookURL,
             
             function(packageDocument, options){
+                
+                if (!packageDocument) {
+                    
+                    console.error("ERROR OPENING EBOOK: " + ebookURL_filepath);
+                    
+                    spin(false);
+                    setBookTitle(ebookURL_filepath);
+                            
+                    Dialogs.showErrorWithDetails(Strings.err_epub_corrupt, ebookURL_filepath);
+                    //Dialogs.showModalMessage(Strings.err_dlg_title, ebookURL_filepath);
+                            
+                    return;
+                }
+                
                 currentPackageDocument = packageDocument;
                 currentPackageDocument.generateTocListDOM(function(dom){
                     loadToc(dom)
@@ -89,8 +112,7 @@ Helpers){
                 wasFixed = readium.reader.isCurrentViewFixedLayout();
                 var metadata = options.metadata;
     
-                $('<h2 class="book-title-header"></h2>').insertAfter('.navbar').text(metadata.title);
-    
+                setBookTitle(metadata.title);
     
                 $("#left-page-btn").unbind("click");
                 $("#right-page-btn").unbind("click");
@@ -108,29 +130,44 @@ Helpers){
         );
     };
 
-    var spin = function()
+    var spin = function(on)
     {
-//console.error("do SPIN: -- WILL: " + spinner.willSpin + " IS:" + spinner.isSpinning + " STOP REQ:" + spinner.stopRequested);
-        if (spinner.willSpin || spinner.isSpinning) return;
-
-        spinner.willSpin = true;
-
-        setTimeout(function()
-        {
-            if (spinner.stopRequested)
+        if (on) {
+    //console.error("do SPIN: -- WILL: " + spinner.willSpin + " IS:" + spinner.isSpinning + " STOP REQ:" + spinner.stopRequested);
+            if (spinner.willSpin || spinner.isSpinning) return;
+    
+            spinner.willSpin = true;
+    
+            setTimeout(function()
             {
-//console.debug("STOP REQUEST: -- WILL: " + spinner.willSpin + " IS:" + spinner.isSpinning + " STOP REQ:" + spinner.stopRequested);
+                if (spinner.stopRequested)
+                {
+    //console.debug("STOP REQUEST: -- WILL: " + spinner.willSpin + " IS:" + spinner.isSpinning + " STOP REQ:" + spinner.stopRequested);
+                    spinner.willSpin = false;
+                    spinner.stopRequested = false;
+                    return;
+                }
+    //console.debug("SPIN: -- WILL: " + spinner.willSpin + " IS:" + spinner.isSpinning + " STOP REQ:" + spinner.stopRequested);
+                spinner.isSpinning = true;
+                spinner.spin($('#reading-area')[0]);
+    
                 spinner.willSpin = false;
-                spinner.stopRequested = false;
-                return;
+    
+            }, 100);
+        } else {
+            
+            if (spinner.isSpinning)
+            {
+//console.debug("!! SPIN: -- WILL: " + spinner.willSpin + " IS:" + spinner.isSpinning + " STOP REQ:" + spinner.stopRequested);
+                spinner.stop();
+                spinner.isSpinning = false;
             }
-//console.debug("SPIN: -- WILL: " + spinner.willSpin + " IS:" + spinner.isSpinning + " STOP REQ:" + spinner.stopRequested);
-            spinner.isSpinning = true;
-            spinner.spin($('#reading-area')[0]);
-
-            spinner.willSpin = false;
-
-        }, 100);
+            else if (spinner.willSpin)
+            {
+//console.debug("!! SPIN REQ: -- WILL: " + spinner.willSpin + " IS:" + spinner.isSpinning + " STOP REQ:" + spinner.stopRequested);
+                spinner.stopRequested = true;
+            }
+        }
     };
 
     var tocShowHideToggle = function(){
@@ -273,18 +310,7 @@ Helpers){
             savePlace();
             updateUI(pageChangeData);
 
-
-            if (spinner.isSpinning)
-            {
-//console.debug("!! SPIN: -- WILL: " + spinner.willSpin + " IS:" + spinner.isSpinning + " STOP REQ:" + spinner.stopRequested);
-                spinner.stop();
-                spinner.isSpinning = false;
-            }
-            else if (spinner.willSpin)
-            {
-//console.debug("!! SPIN REQ: -- WILL: " + spinner.willSpin + " IS:" + spinner.isSpinning + " STOP REQ:" + spinner.stopRequested);
-                spinner.stopRequested = true;
-            }
+            spin(false);
 
             if (!_tocLinkActivated) return;
             _tocLinkActivated = false;
@@ -368,7 +394,7 @@ Helpers){
         $('#readium-toc-body').on('click', 'a', function(e)
         {
             try {
-                spin();
+                spin(true);
     
                 var href = $(this).attr('href');
                 href = tocUrl ? new URI(href).absoluteTo(tocUrl).toString() : href;
@@ -709,7 +735,7 @@ Helpers){
         $('#zoom-custom a').on('click', enableCustom);
         $('.zoom-wrapper input').on('change', setCustom);
 
-        spin();
+        spin(true);
     }
 
     var loadReaderUI = function (data) {
@@ -894,7 +920,7 @@ Helpers){
             Keyboard.on(Keyboard.NightTheme, 'reader', toggleNightTheme);
 
             readium.reader.on(ReadiumSDK.Events.CONTENT_DOCUMENT_LOAD_START, function($iframe, spineItem) {
-                spin();
+                spin(true);
             });
 
             EpubReaderMediaOverlays.init(readium);
@@ -1005,7 +1031,14 @@ Helpers){
         // visibility check fails because iframe is unloaded
         //if (readium.reader.isMediaOverlayAvailable())
         if (readium && readium.reader) // window.push/popstate
-            readium.reader.pauseMediaOverlay();
+        {
+            try{
+                readium.reader.pauseMediaOverlay();
+            }catch(err){
+                //ignore error.
+                //can occur when ReaderView._mediaOverlayPlayer is null, for example when openBook() fails 
+            }
+        }
 
         $(window).off('resize');
         $(window).off('mousemove');
