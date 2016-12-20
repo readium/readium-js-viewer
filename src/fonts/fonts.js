d03ae37c80777cb4b0bd4109c6c2e5e32cb23fcd
danielweck <daniel.weck@gmail.com>
2016-12-20 17:56:25 +0000

    font face configuration now detects Chrome Packaged App in order to automically exclude online web fonts (Google CDN), which is forbidden due to Content Security Policy.

--- src/fonts/fonts.js
+++ src/fonts/fonts.js
@@ -1,4 +1,4 @@
-var _fontFaces =
+var _fontFaces_BUILT_IN =
 [
     {
         displayName: "Open Dyslexic",
@@ -14,7 +14,11 @@ var _fontFaces =
         displayName: "Go Mono (serif)",
         fontFamily: "Go Mono",
         url: "Go/GoMono.css"
-    },
+    }
+];
+
+var _fontFaces_ONLINE =
+[
     {
         displayName: "Open Sans (sans-serif) [online]",
         fontFamily: "Open Sans",
@@ -36,6 +40,25 @@ var _fontFaces =
         url: "https://fonts.googleapis.com/css?family=Bitter"
     }
 ];
+
+//////////////
+// TO CUSTOMIZE AVAILABLE FONT FACES IN YOUR READIUM APP, DO NOT CHANGE THE CODE BELOW, SIMPLY EDIT THE ABOVE ARRAYS.
+//////////////
+
+
+
+var _fontFaces = _fontFaces_BUILT_IN;
+
+// Due to CSP, Chrome Packaged Apps cannot include references to online web fonts
+var isChromeExtensionPackagedApp = (typeof chrome !== "undefined") && chrome.app;
+//&& chrome.app.window && chrome.app.window.current; // a bit redundant?
+
+if (!isChromeExtensionPackagedApp) {
+    for (var i = 0; i < _fontFaces_ONLINE.length; i++) {
+        var fontFace = _fontFaces_ONLINE[i];
+        _fontFaces.push(fontFace);
+    }
+}
 
 // function exported to global context (window or self) when this fonts.js file is linked from index.html of cloud reader, Chrome app, etc.
 var getFontFaces = function(URLprefix) {
